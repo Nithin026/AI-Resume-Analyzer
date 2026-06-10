7492fdba1f4a1cce22dbf0225a90d1d6cf012166
Nithin026 <knithinkumar73@gmail.com>
2026-06-10 16:11:14 +0530

    Update CORS for deployment

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://ai-resume-analyzer-two-tau.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
